73cfb30b52a12b8df157c72e8f3f1f22051fe1fa
Mixathon <vincent.hu@epita.fr>
2020-08-05 13:33:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def endorse(driver):
     """
     wait = WebDriverWait(driver, delay)
     try:
-        wait.until(EC.presence_of_element_located((By.ID, "profile-content")))
+        wait.until(EC.visibility_of_element_located((By.ID, "profile-content")))
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight / 2);")
     except TimeoutException:
         print("Error on scroll to load skills.")
